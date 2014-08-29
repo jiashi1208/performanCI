684e3e29c51349d2ba7395f185b63a4cd7484845
jiashi1208 <stone0911@qq.com>
2014-08-29 03:59:44 +0100

    optimize script,support chinese

---
+++ DataStatistics2.awk
@@ -0,0 +1,77 @@
+BEGIN{
+print "version "version
+print "vertime "vertime
+print "date "date
+}
+{
+if(ARGIND==1) {array1[$1]=$2}
+if(ARGIND==2) {array2[$1]=$2}
+k=0;
+}
+END{
+ 
+   for(key in array1){
+      i=index(tolower(key),"max")
+	  j=index(tolower(key),"power")
+	  k=index(tolower(key),"net")
+	  
+	  if(i!=0 ||j!=0||k!=0){
+	  
+	     #排除比较电量，网络，最高速度等。
+	     test_result="pass"
+	     describe="data_is_ok数据正确"
+	  
+	  }else {
+	  
+	    #没有值，为-1
+	    if(match(array1[key],"-1.00")||match(array1[key],"-1")){
+		  test_result="fail"
+	      describe="data_is_null数据为空" 
+		  summary=key"_value_is_null"
+		  print summary > "error.txt"
+		  
+		  url="http://180.149.144.140/productci/index2.php?act=insert_CIPerformanceDetail&version="version"&testname="key"&testitem="key"&testdata="array1[key]"&testresult="test_result"&testdetail=describetestdetail&timestamp="date"&describe="describe"&vertime="vertime
+	      print url > "url.txt"
+		  continue
+		}
+		if(compare(array1[key],array2[key])==1){
+		
+		 test_result="pass"
+	     describe="data_is_ok"
+		
+		}else if(compare(array1[key],array2[key])==0){
+		
+		 test_result="fail"
+	     describe="data_uncorrect_far_away_from_standard_value"
+		 summary=key"_test_data_uncorrect_far_away_from_expected"
+		 print summary > "error.txt"
+		}
+	  }
+	  
+	  url="http://180.149.144.140/productci/index2.php?act=insert_CIPerformanceDetail&version="version"&testname="key"&testitem="key"&testdata="array1[key]"&testresult="test_result"&testdetail=describetestdetail&timestamp="date"&describe="describe"&vertime="vertime
+	  print url > "url.txt"
+ 
+   }
+
+ }
+
+function compare(value1,value2){
+
+   v1=value1+0;
+   v2=value2+0;
+   
+   if(v2!=0){
+   diff=v1-v2
+   diff_percent=diff/v2
+   diff_percent=diff_percent*100
+   if(diff_percent<-20 || diff_percent>20)
+     { 
+	  return 0
+	  }
+  else{
+      return 1
+	  }
+	}else {
+	return 1
+}
+}
--- test.sh
+++ test.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 #process totalResult.csv
+
+#url encode function
+function urlEncode(){
+echo ${1} | od -t x |awk '{
+    w=split($0,linedata," ");
+    for (j=2;j<w+1;j++)
+    {
+        for (i=7;i>0;i=i-2)
+        {
+            if (substr(linedata[j],i,2) != "00") {printf "%%" ;printf toupper(substr(linedata[j],i,2));}
+
+        }
+    }
+}'
+}
+
+
 date=$1
 version=$2
 vertime=$3
+mkdir $date
 
 php CompareData_no_op.php
 
@@ -10,11 +28,14 @@ cp config.txt $date
 cp Result_reference.txt $date
 rm config.txt
 
+#if [ 1 -eq 2 ]; then 
 #process config.txt,make sure which data is fail,which data is pass
 cd $date
-awk -v version="$version" -v vertime="$vertime" -v date="$date" -F "=" -f ../DataStatistics.awk config.txt Result_reference.txt
+awk -v version="$version" -v vertime="$vertime" -v date="$date" -F "=" -f ../DataStatistics2.awk config.txt Result_reference.txt
 
 sleep 3
+
+#fi
 
 #estimate summary info
 if [ -f error.txt ]
@@ -31,14 +52,28 @@ if [ -f error.txt ]
   describe="test_pass_everyvalue_correct"
 fi
 #no runtime
-url="http://10.95.36.21:8151/utest/lijie/ci/index2.php?act=insert_CIPerformance&version="$version"&testname=android_performance_test&testresult="$test_result"&fromplatform=android&timestamp="$date"&runtime=201408121448&describe="$describe"&vertime="$vertime
+url="http://180.149.144.140/productci/index2.php?act=insert_CIPerformance&version="$version"&testname=android_performance_test&testresult="$test_result"&fromplatform=android&timestamp="$date"&runtime="$date"&describe="$describe"&vertime="$vertime
 echo $url >>url.txt
 
-#send http request
-cat url.txt |while read line
+#对字符串进行编码
+while read line
 do
- curl  $line
+  a=$( expr $line : '.*&describe=\(.*\)&vertime' )
+  b=$(urlEncode  $a);
+  line2=${line//"$a"/"$b"}
+  echo $line2 >>new_url.txt
+ 
+done <url.txt
+
+#send http request
+#if [ 1 -eq 2 ];then 
+cat new_url.txt |while read line
+do
+ curl  "$line"
  sleep 2
 done
 
+#fi
 cd ..
+
+
